9b7bcfc8423983b0b0f94044cfec3248c7652847
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-04-18 17:50:49 +0300

    Update DOM-Operations.js

--- DOM-Operations/DOM-Operations.js
+++ DOM-Operations/DOM-Operations.js
@@ -1,1 +1,40 @@
+/*jslint devel: true */
+/*jslint es6 */
+'use strict';
 
+function solve(el, arr) {
+    if (arguments.length <= 1) {
+        throw new Error('Missing function arguments');
+    } else if (typeof el === typeof '') {
+        if (!(document.getElementById(el) instanceof Element)) {
+            throw new Error(`Invalid ID: ${el}`);
+        }
+    } else if (!(el instanceof Element)) {
+        throw new Error(`Invalid DOM element: ${el}`);
+    } else if (!Array.isArray(arr)) {
+        throw new Error(`${arr} is not a type of array`);
+    }
+    let isArgsValid = true;
+    arr.forEach(element => {
+        if (typeof element === typeof '' || !isNaN(element)) {
+        } else {
+            isArgsValid = false;
+        }
+    });
+    if (!isArgsValid) {
+        return el;
+    }
+    let element = el;
+    if (typeof el === typeof '') {
+        element = document.getElementById(el);
+    }
+    element.innerHTML = '';
+
+    let docFragment = document.createDocumentFragment();
+    arr.forEach(value => {
+        let div = document.createElement('div');
+        div.innerHTML = value;
+        docFragment.appendChild(div);
+    });
+    element.appendChild(docFragment);
+}
