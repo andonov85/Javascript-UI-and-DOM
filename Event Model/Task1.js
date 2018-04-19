4fe698f1c5ba5d4f5b3eb6b67305309db93bc791
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-04-19 19:00:47 +0300

    Update Task1.js

--- Event Model/Task1.js
+++ Event Model/Task1.js
@@ -1,1 +1,26 @@
+/*jslint devel: true */
+/*jslint es6 */
+'use strict';
 
+function solve(el) {
+    if (arguments.length === 0) {
+        throw new Error('Expected 1 function argument');
+    } else if (typeof el === 'string') {
+        if (document.getElementById(el) === null) {
+            throw new Error(`ID:\'${el}\' doesn\'t exist`);
+        }
+    } else if (!(el instanceof Node)) {
+        throw Error(`${el} is not DOM element`);
+    } else if (!document.body.contains(el)) {
+        throw Error(`${el} is non-existing in document`);
+    }
+    const element = el;
+    if (typeof el === 'string') {
+        element = document.getElementById(el);
+    }
+    const buttons = element.getElementsByClassName('button');
+    const contents = element.getElementsByClassName('content');
+    buttons.forEach(element => {
+        element.hidden = true;
+    });
+}
