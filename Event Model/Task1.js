407054e69c4e8b2968e423824113beea2b14982c
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-04-20 17:55:52 +0300

    Update Task1.js

--- Event Model/Task1.js
+++ Event Model/Task1.js
@@ -10,17 +10,39 @@ function solve(el) {
             throw new Error(`ID:\'${el}\' doesn\'t exist`);
         }
     } else if (!(el instanceof Node)) {
-        throw Error(`${el} is not DOM element`);
+        throw new Error(`${el} is not DOM element`);
     } else if (!document.body.contains(el)) {
-        throw Error(`${el} is non-existing in document`);
+        throw new Error(`${el} is non-existing in document`);
     }
-    const element = el;
+    let element = el;
     if (typeof el === 'string') {
         element = document.getElementById(el);
     }
     const buttons = element.getElementsByClassName('button');
-    const contents = element.getElementsByClassName('content');
-    buttons.forEach(element => {
-        element.hidden = true;
+    Array.from(buttons).forEach(element => {
+        element.innerHTML = 'hide';
     });
+    element.addEventListener("click", function(event) {
+        if (event.target.className === 'button') {
+            let currTarget = event.target;
+            while (currTarget.nextElementSibling !== null
+                    && currTarget.nextElementSibling.className !== 'content') {
+                if (currTarget.nextElementSibling.className === 'button') {
+                    return;
+                }
+                currTarget = currTarget.nextElementSibling;
+            }
+            if (currTarget.nextElementSibling === null) {
+                return
+            }
+            currTarget = currTarget.nextElementSibling;
+            if (currTarget.style.display === '') {
+                currTarget.style.display = 'none';
+                event.target.innerHTML = 'show';
+            } else if (currTarget.style.display === 'none') {
+                currTarget.style.display = '';
+                event.target.innerHTML = "hide";
+            }
+        }
+    }, false);
 }
