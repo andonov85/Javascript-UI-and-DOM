662e1cc1cd8574b5533c7a3d935269e5e2d0ebc4
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-04-27 19:19:57 +0300

    Update Task-1.js

--- jQuery Plugins/Task-1.js
+++ jQuery Plugins/Task-1.js
@@ -18,19 +18,51 @@ function dropdownList(selector) {
     el.hide();
     $(el).after('<div class="dropdown-list"></div>');
     $('.dropdown-list').append(el);
-    $('.dropdown-list').append('<div class="current" data-value="">'
-                                + $('#the-select').find('option').first().text() + '</div>');
-    let container = $('<div>').css({
-        'class': 'options container',
-        'style': 'position: absolute',
-        'display': 'none'
+    $('.dropdown-list').append('<div class="current" data-value="">' +
+        $('#the-select').find('option').first().text() + '</div>');
+    $('.current').css({
+        'background': 'gainsboro',
+        'position': 'absolute',
+        'border': 'none',
+        'padding': '5px 10px',
+        'cursor': 'context-menu'
+    });
+    let container = $('<div>').attr({
+        'class': 'options-container',
+        'style': `position: absolute;
+                  top: 38px;
+                  display: none;
+                  background-color: grey;
+                  color: white`
     });
     $('#the-select').find('option').each(function (index) {
         container.append('<div class=\'dropdown-item\'>');
-        $(container.find('.dropdown-item')[index]).css({
-                    'data-value': '$(this).value',
-                    'data-index': 'index'
-                 }).text($(this).text());
+        $(container.find('.dropdown-item')[index]).attr({
+            'data-value': $(this).attr('value'),
+            'data-index': index,
+            'style': 'padding: 5px 10px; cursor: context-menu'
+        }).text($(this).text());
     });
     $('.dropdown-list').append(container);
+
+    $('.current').on('click', function () {
+        $('.options-container').toggle();
+    });
+    $('.dropdown-item').on('click', function () {
+        $('.options-container').hide();
+        $('.current').attr({
+            'data-value': $(this).attr('data-value')
+        })
+        $('.current').text($(this).text());
+    });
+    $('.dropdown-item').on('mouseover', function () {
+        $(this).css({
+            'background': 'coral'
+        });
+    });
+    $('.dropdown-item').on('mouseleave', function () {
+        $(this).css({
+            'background': 'grey'
+        });
+    });
 }
