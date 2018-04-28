e44d80a92354080b83b624d9eb49671ee2ade2e3
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-04-28 19:33:31 +0300

    Update main.js

--- jQuery Plugins/ColorPicker/main.js
+++ jQuery Plugins/ColorPicker/main.js
@@ -4,6 +4,20 @@
 
 (function ($) {
     $.fn.colorpicker = function () {
-        $(this).html('<figure>').src();
+        // Creating small bucket tumbnail
+        let $this = $(this);
+        $this.html('<img id="smallbucket">');
+        $this.find('#smallbucket')
+               .attr({
+            src: 'bucket_thumbnail.png',
+            width: '30',
+            height: '30'
+        });
+        $('#smallbucket').on('click', function() {
+            $this.append('<section id="color-picker">');
+            $('#color-picker').append('<input id="hex-input">');
+            $('#color-picker').append('<input id="rgb-input">');
+            $('#color-picker').append('<aside id="current-color">');
+        });
     }
 }(jQuery));
