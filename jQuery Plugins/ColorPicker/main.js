fca6eafa9bb31d1d3f5f085c979a41f2f1e2b17f
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-04-29 13:02:56 +0300

    Update main.js

--- jQuery Plugins/ColorPicker/main.js
+++ jQuery Plugins/ColorPicker/main.js
@@ -4,20 +4,37 @@
 
 (function ($) {
     $.fn.colorpicker = function () {
-        // Creating small bucket tumbnail
+        // Creating bucket tumbnail
         let $this = $(this);
-        $this.html('<img id="smallbucket">');
+        $this.html('');
+        $this.append('<img id="image-pallete">');
+        $this.find('#image-pallete')
+               .attr({
+                    src: 'color-wheel.jpg',
+                    width: '200',
+                    height: '200'
+                })
+                .hide();
+        $this.append('<img id="smallbucket">');
         $this.find('#smallbucket')
                .attr({
-            src: 'bucket_thumbnail.png',
-            width: '30',
-            height: '30'
+                    src: 'bucket_thumbnail.png',
+                    width: '30',
+                    height: '30'
         });
+        //
         $('#smallbucket').on('click', function() {
             $this.append('<section id="color-picker">');
-            $('#color-picker').append('<input id="hex-input">');
-            $('#color-picker').append('<input id="rgb-input">');
-            $('#color-picker').append('<aside id="current-color">');
+            $('#color-picker').append('<canvas id="canvas-pallete">')
+                              .append('<input id="hex-input">')
+                              .append('<input id="rgb-input">')
+                              .append('<aside id="current-color">');
+            let canvas = document.getElementById('canvas-pallete');
+            let ctx = canvas.getContext('2d');
+            ctx.canvas.width = 230;
+            ctx.canvas.height = 230;
+            let img = document.getElementById('image-pallete');
+            ctx.drawImage(img, 10, 10, 210, 210);
         });
     }
 }(jQuery));
