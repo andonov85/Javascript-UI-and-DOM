15d5f3cc5c8132bc31843d17ac0960017fc0a07a
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-05-03 15:10:02 +0300

    Update main.js

--- jQuery Plugins/Lists-Task-3/main.js
+++ jQuery Plugins/Lists-Task-3/main.js
@@ -3,7 +3,7 @@
 'use strict';
 let lists = [
     ['Names', 'gosho', 'todor', 'ivan', 'kaloyan', 'gergana', 'john'],
-    ['Laguages', 'french', 'english', 'german', 'spanish', 'russian', 'javascript'],
+    ['Languages', 'french', 'english', 'german', 'spanish', 'russian', 'javascript'],
     ['Cool things', 'icecream', 'dancing', 'party', 'beer', 'programming', 'diving', 'reading', 'learning']
 ];
 (function ($) {
@@ -12,17 +12,23 @@ let lists = [
         // Following must be done lists.length times
         $this.html('');
         for (let i = 0; i < list.length; i += 1) {
-            $this.append('<table id="Table-' + (i + 1) + '">');
+            $this.append('<table id="Table-' + (i + 1) + '" style="width:100px">');
             let table = $this.find('#Table-' + (i + 1));
-            table.css('display', 'inline');
+            table.css({
+                'display': 'inline',
+                'background': 'lightblue'
+            }).attr({
+                // 'border': '1px'
+            });
             list[i].forEach(function (value, index) {
                 if (index === 0) {
                     table.append('<tr><th>');
                     table.find('th').text(value);
                     table.append('<tr><td><button>');
-                    table.find('button').after('<input type="text">');
+                    table.find('button').after('<input type="text" style="float: right; width: 180px">');
+                    table.append('<tbody>');
                 } else {
-                    table.append('<tr><td><a>');
+                    table.append('<tr><td style="width: 180px; text-align: center; border: 1px solid"><a style="text-decoration: none">');
                     table.find('a').last().attr({
                         target: '_blank',
                         href: 'https://www.google.com/search?q=' + value
@@ -54,32 +60,36 @@ let lists = [
             }
         })
         // Draging and droping
-        $this.find('a').css({
+        $this.find('tr').css({
             '-moz-user-select': 'none', // moz ff
             'user-select': 'none',
-            'position': 'relative'
+            // 'position': 'relative'
         }).attr({
             'ondragstart': 'return false;', // moz ff
             'draggable': 'false'
         });
         $this.on('mousedown', function (event) {
-            if ($(event.target).is('a')) {
-                let offset = $(event.target).offset();
+            if (event.target.tagName === 'TD') {
+                let $tr = $(event.target).parent();
+                let $td = $(event.target);
+                let offset = $td.offset();
                 let dx = event.pageX - offset.left;
                 let dy = event.pageY - offset.top;
-                $(event.target).on('mousemove', function (event) {
-                    $(this).css({
+                $td.css('position', 'absolute');
+                $(document).on('mousemove', function (event) {
+                    $td.css({
                         left: event.pageX - dx,
                         top: event.pageY - dy
                     });
-                    console.log(event.pageX + ' ' + event.pageY);
-                    console.log(dx + ' ' + dy);
                 });
-                $(event.target).on('mouseup', function () {
-                    $(this).off('mousemove');
+                $this.on('mouseup', function (event) {
+                    $(document).off('mousemove');
+                    if (event.target.tagName === 'TD') {
+                    }
+                    
                 });
             }
-        });
-    }
+        }
+    )};
 }(jQuery));
 $('.container').lists(lists);
